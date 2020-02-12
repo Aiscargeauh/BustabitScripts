b56d59bb5219138c7f7ce7895a67f063117e13d2
Nicolas <nicolasruhland@gmail.com>
2020-02-12 22:55:42 +0100

    Add a requested script

---
+++ AngryRabbitHole.js
@@ -0,0 +1,15 @@
+//Only an idea for the moment
+//Will implement soon
+
+//Base turns would be:
+//-->base bet @ x2
+//-->if win, restart
+
+//if base turn is lost, bet a turn to get back the lost bits and make a bit of profit:
+//-->last bet * 6 @ x1.25
+
+//if the previous turn is lost, go down the rabbit hole:
+//-->last bet * 60.5% @ x3
+
+//Continue turns until safety is reached (safety is an input from the user, it's how deep we go down in the rabbit hole)
+//if safety is reached, go back to base turns
