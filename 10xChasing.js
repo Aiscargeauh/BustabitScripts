ce6d977d8e53ddfcf37bac6ee847e76813d8cc20
Nicolas <nicolasruhland@gmail.com>
2020-02-12 22:12:00 +0100

    Adding current scripts

---
+++ 10xChasing.js
@@ -0,0 +1,133 @@
+var config = {
+	baseBet: { value: 3200, type: 'balance', label: 'Base bet'},
+	gamesToWait: { value: 25, type: 'text', label: 'Games to wait before making a bet'}
+}
+
+//10x chasing script by @Cannonball
+//Feel free to tip, as is it a free script
+//Also feel free to ping me if you got questions
+//Will wait for the number of game you set before beginning the chase
+//->Will bet 9 games at the base bet you set
+//-->Will double the bet after 9 lost games
+//--->Will double the bet after 6 lost games (15 total)
+//---->Will double the bet every 5 games then (20, 25, 30...) until 10x
+//Has a history feature, will check the current number of games since 10x at the beginning of script
+//Has a simulation feature, will calculate how many games without 10x you can handle without busting your bankroll
+//Has logging functionalities, press F12
+
+var gamesWithout10 = GetGamesWithout10();
+var numberOf10xCashedOut = 0;
+var userProfitInSatoshis = 0;
+var currentBetInSatoshis = config.baseBet.value;
+var isBettingNow = false;
+var loosingStreak = 0;
+var gamesToBeSafy = 130;
+var biggestBet = 0;
+
+var gamesTheBotCanHandle = CalculateBotSafeness(config.baseBet.value, config.gamesToWait.value);
+log('FIRST LAUNCH | WELCOME!');
+log('Bot safety check :');
+log('-> You can manage to loose ' + gamesTheBotCanHandle + ' games without 10x before busting to zero');
+log('-> With the maximum bet: ' + biggestBet / 100 + 'bits.');
+log('-> We do assume 130 games is the maximum streak without 10x so...');
+if(gamesTheBotCanHandle >= gamesToBeSafy){
+	log('--> It looks safe with your parameters, let\'s go!');	
+}else{
+	log('--> Please stay around, it\'s not really safe with your parameters, chances to bust are quite high...');
+}
+
+engine.on('GAME_STARTING', function() {
+	log('');
+	log('NEW GAME')
+	log('Games since no 10x: ' + gamesWithout10 + '. You can handle: ' + gamesTheBotCanHandle + ' games without 10x.');
+	log('Actual profit using the script: ' + userProfitInSatoshis /100 + ' bits. Got ' + numberOf10xCashedOut + ' times 10x.');
+	if(gamesWithout10 > config.gamesToWait.value){
+		//do place bet
+		engine.bet(currentBetInSatoshis, 10);
+		let currentBetInBits = currentBetInSatoshis / 100;
+		let wantedProfit = (currentBetInBits * 9) + (userProfitInSatoshis / 100);
+		log('Betting ' + currentBetInBits + ' right now, looking for ' + wantedProfit + ' bits total profit.')
+		isBettingNow = true;
+	}else{
+		isBettingNow = false;
+		let calculatedGamesToWait = config.gamesToWait.value - gamesWithout10;
+		if(calculatedGamesToWait == 0){
+			log('Betting ' + config.baseBet.value/100 + 'bit(s) next game!');
+		}else{
+			log('Waiting for ' + calculatedGamesToWait + ' more games with no 10x');
+		}
+	}
+})
+
+engine.on('GAME_ENDED', function() {
+	let gameInfos = engine.history.first();
+	if(isBettingNow){
+		if(!gameInfos.cashedAt){
+			userProfitInSatoshis -= currentBetInSatoshis;
+			loosingStreak++;
+			if(loosingStreak == 9){
+				currentBetInSatoshis *= 2;
+			}
+			if(loosingStreak > 10 && loosingStreak % 5 == 0){
+				currentBetInSatoshis *= 2;
+			}
+		}else if(gameInfos.cashedAt){
+			numberOf10xCashedOut++;
+			loosingStreak = 0;
+			userProfitInSatoshis = userProfitInSatoshis + (currentBetInSatoshis * 9);
+			currentBetInSatoshis = config.baseBet.value;
+		}
+	}
+	if(gameInfos.bust > 10){
+		gamesWithout10 = 0;
+	}else{
+		gamesWithout10++;
+	}
+	log('END GAME');
+})
+
+function CalculateBotSafeness(baseBetForBot, gamesToWaitForBot){
+	//22:06 Cannonball: !streak < 10
+	//22:06 Shiba: Seen 122 streak in games #1801655 - #1801776: 1.25x, 2.13x, 3.25x, 5.57x, 1.62x, 5.45x, 1.43x, 8.75x... (59,226 games ago, 15d 9h 10m 43s ago)
+	//Let's assume if we can handle 130 games, we're safe
+	//And let's simulate
+
+	let gamesToBeSafe = 130;
+	let totalGames = gamesToWaitForBot;
+	let brInSatoshis = userInfo.balance;
+	let bettedGames = 0;
+	let nextBet = baseBetForBot;
+	let broken = false;
+	let totalBet = 0;
+
+	while(!broken){
+		brInSatoshis -= nextBet;
+		totalGames++;
+		bettedGames++;
+		totalBet += nextBet;
+		if (bettedGames % 9 == 0 && bettedGames < 10) 
+		{
+			nextBet *= 2;
+		}else if(bettedGames % 5 == 0 && bettedGames > 10){
+			nextBet *= 2;
+		}
+		if(nextBet > brInSatoshis){
+			biggestBet = nextBet;
+			broken = true;
+		}
+	}
+	return totalGames;
+}
+
+function GetGamesWithout10(){
+	let gamesArray = engine.history.toArray();
+	let generatedGamesWithout10 = 0;
+
+	for (var i = 0; i <= gamesArray.length; i++) {
+		if(gamesArray[i].bust >= 10){
+			break;
+		}
+		generatedGamesWithout10++;
+	}
+	return generatedGamesWithout10;
+}
---
+++ 2xChasing.js
@@ -0,0 +1,116 @@
+var config = {
+    baseBet: { value: 100, type: 'balance', label: 'Base bet' },
+    redStreakToWait: { value: 10, type: 'text', label: 'Red games to wait before making a bet' }
+}
+
+//2x chasing script by @Cannonball
+//Feel free to tip, as is it a free script
+//Also feel free to ping me if you got questions
+//Will wait for the number of game you set before beginning a martingale
+//->Will wait for the red streak (from input)
+//-->Will double the bet every game lost
+//Has a history feature, will check the current number of red games when starting the script to bet accordingly
+//Has a simulation feature, will calculate how many games without green you can handle before busting your bankroll
+//Has logging functionalities, press F12
+
+var biggestBet = 0;
+var currentRedStreak = GetCurrentRedStreak();
+var gamesTheBotCanHandle = CalculateBotSafeness(config.baseBet.value, config.redStreakToWait.value);
+var userProfitInSatoshis = 0;
+var numberOf2xCashedOut = 0;
+var currentBetInSatoshis = config.baseBet.value;
+var isBettingNow = false;
+var gamesToBeSafy = 25;
+
+log('FIRST LAUNCH | WELCOME!');
+log('Bot safety check :');
+log('-> You can manage to loose a ' + gamesTheBotCanHandle + ' red streak.');
+log('-> The maximum bet would be: ' + biggestBet/100 + ' bits.');
+log('-> We do assume 25 games is the maximum streak without 2x so...');
+if (gamesTheBotCanHandle >= gamesToBeSafy) {
+    log('--> It looks safe with your parameters, let\'s go!');
+} else {
+    log('--> Please stay around, it\'s not really safe with your parameters, chances to bust are quite high...');
+}
+log('There is a streak of ' + currentRedStreak + " red games now.");
+
+engine.on('GAME_STARTING', function () {
+    log('');
+    log('NEW GAME')
+    log('Games since no 2x: ' + currentRedStreak + '. You can handle: ' + gamesTheBotCanHandle + ' games without 2x.');
+    log('Actual profit using the script: ' + userProfitInSatoshis / 100 + ' bits. Got ' + numberOf2xCashedOut + ' times 2x.');
+    if (currentRedStreak >= config.redStreakToWait.value) {
+        //do place bet
+        engine.bet(currentBetInSatoshis, 2);
+        let currentBetInBits = currentBetInSatoshis / 100;
+        let wantedProfit = currentBetInBits + (userProfitInSatoshis / 100);
+        log('Betting ' + currentBetInBits + ' right now, looking for ' + wantedProfit + ' bits total profit.')
+        isBettingNow = true;
+    } else {
+        isBettingNow = false;
+        let calculatedGamesToWait = config.redStreakToWait.value - currentRedStreak;
+        if (calculatedGamesToWait <= 1) {
+            log('Betting ' + config.baseBet.value / 100 + 'bit(s) next game (if red)!');
+        } else {
+            log('Waiting for ' + calculatedGamesToWait + ' more games with no 2x');
+        }
+    }
+})
+
+engine.on('GAME_ENDED', function () {
+    let gameInfos = engine.history.first();
+    if (isBettingNow) {
+        if (!gameInfos.cashedAt) {
+            userProfitInSatoshis -= currentBetInSatoshis;
+            currentBetInSatoshis *= 2;
+        } else if (gameInfos.cashedAt) {
+            numberOf2xCashedOut++;
+            userProfitInSatoshis = userProfitInSatoshis + currentBetInSatoshis;
+            currentBetInSatoshis = config.baseBet.value;
+        }
+    }
+    if (gameInfos.bust > 2) {
+        currentRedStreak = 0;
+    } else {
+        currentRedStreak++;
+    }
+    log('END GAME');
+})
+
+function CalculateBotSafeness(baseBetForBot, gamesToWaitForBot) {
+    //15:41 Cannonball: !streak < 2
+    //15:41  Shiba: Seen 20 streak in games #2684941 - #2684960: 1.07x, 1.31x, 1.64x, 1.27x, 1x, 1.14x, 1.81x, 1.52x... (178,392 games ago, 1M 15d 11h 48m 19s ago)
+    //Let's assume if we can handle 25 games, we're safe
+    //And let's simulate
+
+    let totalGames = gamesToWaitForBot;
+    let brInSatoshis = userInfo.balance;
+    let nextBet = baseBetForBot;
+    let broken = false;
+    let totalBet = 0;
+
+    while (!broken) {
+        brInSatoshis -= nextBet;
+        totalGames++;
+        totalBet += nextBet;
+        biggestBet = nextBet;
+        nextBet *= 2;
+        if (nextBet > brInSatoshis) {
+            broken = true;
+        }
+    }
+    return totalGames;
+}
+
+function GetCurrentRedStreak() {
+    let gamesArray = engine.history.toArray();
+    let generatedRedStreak = 0;
+
+    for (var i = 0; i <= gamesArray.length; i++) {
+        if (gamesArray[i].bust >= 2) {
+            break;
+        }
+        generatedRedStreak++;
+    }
+    return generatedRedStreak;
+}
