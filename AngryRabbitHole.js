cb4ad153af886285d54925ec3984c764241fcbe3
Nicolas_AWTCE <nicolas.ruhland@awtce.be>
2020-02-13 11:48:28 +0100

    First thoughts on AngryRabbitHole script

--- AngryRabbitHole.js
+++ AngryRabbitHole.js
@@ -13,3 +13,34 @@
 
 //Continue turns until safety is reached (safety is an input from the user, it's how deep we go down in the rabbit hole)
 //if safety is reached, go back to base turns
+
+
+
+//Config
+var config = {
+    baseBet: { value: 100, type: 'balance', label: 'Base bet' },
+    turnsInRabbitHole: { value: 4, type: 'text', label: 'Turns in the rabbit hole' }
+}
+
+//Variables
+var currentTurn = "base"; //Can be "base", "secondBase", "rabbitHole"
+
+log('FIRST LAUNCH | WELCOME!');
+
+engine.on('GAME_STARTING', function () {
+    log('');
+    log('NEW GAME')
+
+    switch (currentTurn) {
+        case "base":
+            log("Everything is fine, betting normally.");
+            break;
+        case "secondBase":
+            log("Oh no, we lost last game! Trying to recover.");
+            break;
+        case "rabbitHole":
+            log("Everything is on fire, going down the rabbit hole for " + config.turnsInRabbitHole.value + " turns.");
+            break;
+    }
+
+});
