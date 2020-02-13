41e998a0cc9305cf27012a1a86d883b08d92fa08
Nicolas <nicolasruhland@gmail.com>
2020-02-13 20:44:39 +0100

    New GrowingPayout script

---
+++ GrowingPayout.js
@@ -0,0 +1,53 @@
+var config = {
+    baseBet: { value: 100, type: 'balance', label: 'Base Bet' },
+    minimumPayout: { value: 2, type: 'multiplier', label: 'Minimum Payout' },
+    maximumPayout: { value: 100, type: 'multiplier', label: 'Maximum Payout' }
+}
+
+//Limit variable: Being able to set a maximum payout.
+//If reached, go back in reverse to minimize damage.
+
+var currentPayout = 2.15;
+var isBetting = false;
+var userProfit = 0;
+var isGoingUp = true;
+
+engine.on('GAME_STARTING', function () {
+    log('');
+    log('NEW GAME')
+    log('Chasing payout: x' + currentPayout + '.');
+    engine.bet(config.baseBet.value, currentPayout);
+    isBetting = true;
+});
+
+engine.on('GAME_ENDED', function () {
+    let gameInfos = engine.history.first();
+    if(isBetting){
+        if (!gameInfos.cashedAt) {
+            //Lost
+            if(isGoingUp){
+                currentPayout += 1;
+                if(currentPayout >= config.maximumPayout.value && isGoingUp){
+                    isGoingUp = false;
+                    log("Now going down.");
+                }
+            }else{
+                currentPayout -= 1;
+                if(currentPayout <= config.minimumPayout.value && !isGoingUp){
+                    log("Now going up.");
+                    isGoingUp = true;
+                }
+            }
+            userProfit -= config.baseBet.value;
+            log(currentPayout);
+            log('Lost...');
+        }else{
+            //Won
+            userProfit += config.baseBet.value;
+            currentPayout = config.minimumPayout.value;
+            log('Won! Returning to minimum payout.');
+        }
+        log('Current profit: ' + userProfit/100 + ' bits.');
+    }
+    log('END GAME');
+});
--- README.md
+++ README.md
@@ -25,3 +25,7 @@ It will begin to bet only after the number of red games that you input. After th
 The idea of this script is to bet more after a won game, and return to base bet when you lost a game.
 In order to make profit, you can set a limit of won games, to go back to the base bet after a won game. 
 This script makes charts like https://prnt.sc/r1u8jo.
+
+### Growing Payout
+Here instead making the bet grow, we grow the payout.
+There is a minimum and maximum payout to input. If it goes until the maximum, the script is reversing the process to lower the damage.
