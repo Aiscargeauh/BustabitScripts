d3a3096318541157d91d19043040051fb488b0ee
Aiscargeauh <aiscargeauh@protonmail.ch>
2020-08-06 18:39:10 +0200

    fix spacing

--- Bouncy3x.js
+++ Bouncy3x.js
@@ -5,8 +5,8 @@ var config = {
         value: 'fixed', type: 'radio', label: 'Fixed or random waiting time',
         options: {
             fixed: { value: 5, type: 'text', label: 'Fixed cooldown' },
-            random: { value: "3-7", type: 'text', label: 'Random (min-max)'}
-}
+            random: { value: "3-7", type: 'text', label: 'Random (min-max)' }
+        }
     }
 }
 
@@ -31,12 +31,12 @@ engine.on('GAME_STARTING', function () {
     log('');
     log('NEW GAME')
     log('Profit since starting the script: ' + userProfit / 100 + ' bits. Got ' + numberOf3xCashedOut + ' times 3x.');
-    if(cooldown == 0){
+    if (cooldown == 0) {
         engine.bet(currentBet, 3);
         currentStreakBets.push(currentBet);
         log("Betting " + currentBet / 100 + " bits this game.");
         isBettingNow = true;
-    }else{
+    } else {
         log("Cooldown for the next " + cooldown + " games...");
         isBettingNow = false;
     }
@@ -44,26 +44,26 @@ engine.on('GAME_STARTING', function () {
 
 engine.on('GAME_ENDED', function () {
     let gameInfos = engine.history.first();
-    if(isBettingNow){
+    if (isBettingNow) {
         if (!gameInfos.cashedAt) {
             //Lost
             log('Lost...');
             userProfit -= currentBet;
-            if(currentStreakBets.length > 1){
+            if (currentStreakBets.length > 1) {
                 currentBet = currentStreakBets[currentStreakBets.length - 1] + currentStreakBets[currentStreakBets.length - 2];
             }
         } else {
             //Won
             log('Won!');
             userProfit += currentBet * 3;
-            if(config.fixedOrRandom.value == "fixed"){
+            if (config.fixedOrRandom.value == "fixed") {
                 cooldown = config.fixedOrRandom.options.fixed.value;
-            }else{
+            } else {
                 let randomValueStr = config.fixedOrRandom.options.random.value;
                 let dashIndex = randomValueStr.indexOf("-");
                 let min = parseInt(randomValueStr.substring(0, dashIndex));
                 let max = parseInt(randomValueStr.substring(dashIndex + 1, randomValueStr.length));
-                if(isNaN(min) || isNaN(max)){
+                if (isNaN(min) || isNaN(max)) {
                     stop("Cannot parse min and max values. Use min-max, with min and max as numbers");
                 }
                 cooldown = Math.floor(Math.random() * (max - min) + min);
@@ -73,8 +73,8 @@ engine.on('GAME_ENDED', function () {
             currentBet = config.baseBet.value;
             numberOf3xCashedOut++;
         }
-    }else{
-        if(cooldown > 0){
+    } else {
+        if (cooldown > 0) {
             log("Decrementing cooldown...");
             cooldown--;
         }
